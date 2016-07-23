670d4c37d14005ea26a635748c1cd7d684e2f0ec
Valeriy Osipov <info@vosipov.com>
2016-07-24 00:52:14 +0300

    Update telnet commands

--- rate_query_api/api/views.py
+++ rate_query_api/api/views.py
@@ -10,49 +10,27 @@ def exec_telnet_cmd(cmd):
     timeout = current_app.config['TELNET_TIMEOUT']
     nl = '\r\n'
 
-    user = current_app.config['TELNET_USER']
-    password = current_app.config['TELNET_PASSWORD']
     tn = telnetlib.Telnet(current_app.config['TELNET_HOST'], current_app.config['TELNET_PORT'])
-
-    try:
-        tn.read_until("login: ", timeout=timeout)
-    except EOFError as e:
-        return "Connection closed: %s" % e
-
-    tn.write(user + nl)
-    if password:
-        try:
-            tn.read_until("Password: ", timeout=timeout)
-        except EOFError as e:
-            return "Connection closed: %s" % e
-        tn.write(password + nl)
+    tn.write('login%s' % nl)
+    tn.write('help%s' % nl)
 
     try:
         res = tn.read_until('#', timeout=timeout)
     except EOFError as e:
         return "Connection closed: %s" % e
+    res = res.replace('\r\n', '<br/>')
 
-    if 'Login incorrect' in res:
-        return 'Login incorrect'
-
-    tn.write("%s%s" % (cmd, nl))
-
-    try:
-        res = tn.read_until('#', timeout=timeout)
-    except EOFError as e:
-        return "Connection closed: %s" % e
-
-    return res[res.find(nl) + len(nl):res.rfind(nl)]
+    return res
 
 
 @blueprint.route('/api/v1/GetVendorsForDestination', methods=['GET', 'POST'])
 def get_vendors_for_destination():
-    return jsonify({'Result': exec_telnet_cmd('echo "Hello"')})
+    return exec_telnet_cmd('echo "Hello"')
 
 
 @blueprint.route('/api/v1/GetVendorRate', methods=['GET', 'POST'])
 def get_vendor_rate():
-    return jsonify({'Result': exec_telnet_cmd('ps')})
+    return exec_telnet_cmd('ps')
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
